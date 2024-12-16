e3f17450565891126cf5c7440ec395660e208aac
NguyenHoang1602 <sakuraimusic123@gmail.com>
2024-12-16 18:33:14 +0700

    first commit

---
+++ src/constants/appColors.ts
@@ -0,0 +1,9 @@
+/* eslint-disable eol-last */
+/* eslint-disable semi */
+export const appColors = {
+    primary: '#5669FF',
+    white: 'fff',
+    text: '#120D26',
+    grey: '#3807A7A',
+    grey2: '#DADADA',
+}
---
+++ src/screens/index.ts
@@ -0,0 +1,7 @@
+/* eslint-disable eol-last */
+/* eslint-disable quotes */
+import LoginScreen from "./auth/LoginScreen";
+import HomeScreen from "./home/HomeScreen";
+import SplashScreen from "./SplashScreen";
+
+export { HomeScreen, LoginScreen, SplashScreen };
